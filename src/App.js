0920136c61162737d6c2ff6ad3cb6152f0726107
Manas Kumar Panigrahi <82246019+immkp@users.noreply.github.com>
2021-09-04 01:34:03 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Dashboard from "./components/Dashboard";
+import Navbar from './components/Navbar'
+import "./sb-admin-2.min.css";
+
+function App() {
+  return (
+    <>
+      <div id="wrapper">
+        <Navbar />
+        <Dashboard />
+      </div>
+    </>
+  );
+}
+
+export default App;
